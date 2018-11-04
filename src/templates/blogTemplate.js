ec93f7d4087d8e1bfd10afbcfd3e339b0003f663
Tech Uncensored <uncensored@uncensored.tech>
2018-11-04 06:24:03 +0000

    remove type-o in Gatsby documentation

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -23,7 +23,7 @@ export default function Template({
 }
 
 export const pageQuery = graphql`
-  query($path: String!) {
+  query($path: String) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -32,4 +32,5 @@ export const pageQuery = graphql`
         title
       }
     }
-  }`
+  }
+`
